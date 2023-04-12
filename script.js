18cbb002623cfe52012e731fe4dca885cf03a42a
Govindsing Chavan <123160733+govind2611@users.noreply.github.com>
2023-04-12 13:48:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+
+function playsound(sound){
+
+    let audio =   document.querySelector(`audio[src='${sound}']`);
+   
+    console.log(audio);
+    if(audio.paused){
+        audio.play();
+    }
+    else{
+        audio.pause();
+    }
+    
+}
